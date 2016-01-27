9616275763211b6a3e522e85193ee6c2981fb949
Phil <p.clauzel90@gmail.com>
2016-01-27 18:39:34 +0000

    Add job type and job status seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+job_types = [ 'permanent', 'contract', 'full-time', 'part-time', 'freelance' ]
+job_statuses = [ 'open', 'closed', 'paused' ]
+
+job_types.each do |type|
+  JobType.create!(job_type: type)
+end
+
+job_statuses.each do |status|
+  JobStatus.create!(status: status)
+end
