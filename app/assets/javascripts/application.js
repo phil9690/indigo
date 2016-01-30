c6aa8f72262adfdf93e6dff37f1f711dd6ddb4f2
Phil <p.clauzel90@gmail.com>
2016-01-30 16:35:40 +0000

    Redesign indigo v2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,11 +20,53 @@
 
 $(document).on('ready page:load', function () {
   $(document).foundation();
+
+
   $('#banner').backgroundVideo({
     $videoWrap: $('#video-wrap'),
     parallax: true
   });
 
+$('#banner2').backgroundVideo({
+    $videoWrap: $('#video-wrap2'),
+    parallax: true
+  });
+
   ContactForm.init($('#new_hiring_message'), $('#hiring_button'));
   ContactForm.init($('#new_looking_message'), $('#looking_button'));
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 1){
+        $('#nav').addClass('sticky')
+    } else {
+        $('#nav').removeClass('sticky')
+    }
+  });
+});
+
+$(document).ready(function(){
+  var stickyNavTop = $('#nav').offset().top;
+
+  var stickyNav = function() {
+    var scrollTop = $(window).scrollTop();
+    
+    if (scrollTop > 53) {
+      $('body').addClass('scrolled');
+    } else {
+      $('body').removeClass('scrolled');
+    }
+
+    if (scrollTop > stickyNavTop) {
+      $('#nav').addClass('sticky');
+      $('.arrow-down').addClass('hidden');
+    } else {
+      $('#nav').removeClass('sticky');
+    }
+  };
+
+  stickyNav();
+  
+  $(window).scroll(function() {
+    stickyNav();
+  });
 });
